a6cdce89d6aa44f2b0c9d450e2fa5a295c0707c7
Andrew Snodgrass <pointlander@gmail.com>
2025-09-17 13:36:53 -0500

    Generate one markov model per book

--- main.go
+++ main.go
@@ -419,4 +419,67 @@ func main() {
 		_3mMode()
 		return
 	}
+
+	const (
+		size = 256
+	)
+
+	type File struct {
+		Name  string
+		Data  []byte
+		Model Model
+	}
+
+	files := []File{
+		{Name: "pg74.txt.bz2"},
+		{Name: "10.txt.utf-8.bz2"},
+		{Name: "76.txt.utf-8.bz2"},
+		{Name: "84.txt.utf-8.bz2"},
+		{Name: "100.txt.utf-8.bz2"},
+		{Name: "1837.txt.utf-8.bz2"},
+		{Name: "2701.txt.utf-8.bz2"},
+		{Name: "3176.txt.utf-8.bz2"},
+	}
+
+	load := func(book *File) {
+		path := fmt.Sprintf("books/%s", book.Name)
+		file, err := Text.Open(path)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		breader := bzip2.NewReader(file)
+		data, err := io.ReadAll(breader)
+		if err != nil {
+			panic(err)
+		}
+
+		markov := [order]Markov{}
+		for i := range book.Model {
+			book.Model[i] = make(map[Markov][]uint32)
+		}
+		for _, value := range data {
+			for ii := range markov {
+				vector := book.Model[ii][markov[ii]]
+				if vector == nil {
+					vector = make([]uint32, size)
+				}
+				vector[value]++
+				book.Model[ii][markov[ii]] = vector
+
+				state := value
+				for iii, value := range markov[ii][:ii+1] {
+					markov[ii][iii], state = state, value
+				}
+			}
+		}
+		book.Data = data
+	}
+	for i := range files {
+		load(&files[i])
+		fmt.Println(files[i].Name)
+		for ii := range files[i].Model {
+			fmt.Println(len(files[i].Model[ii]))
+		}
+	}
 }
