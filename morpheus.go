5abb854c60a55ada6fd5d99e76f4befd73b84b60
Andrew Snodgrass <pointlander@gmail.com>
2025-09-28 12:42:40 -0500

    New approach

--- morpheus.go
+++ morpheus.go
@@ -238,6 +238,109 @@ func Morpheus2[T any](seed int64, config Config, vectors []*Vector[T], g map[str
 	return cov
 }
 
+func Morpheus3[T any](seed int64, config Config, vectors []*Vector[T]) [][]float64 {
+	rng := RNG(1)
+	rng2 := rand.New(rand.NewSource(1))
+	results := make([][]float64, config.Iterations)
+	width := 2 * config.Size
+	cols, rows := width, width
+	if config.Divider == 0 {
+		rows = int(math.Ceil(math.Log2(float64(width))))
+	} else {
+		rows /= config.Divider
+	}
+	a, b := NewMatrix(cols, rows, make([]float32, cols*rows)...),
+		NewMatrix(cols, rows, make([]float32, cols*rows)...)
+	index := 0
+	for range a.Rows {
+		for range a.Cols {
+			a.Data[index] = float32(rng2.NormFloat64())
+			b.Data[index] = float32(rng2.NormFloat64())
+			index++
+		}
+	}
+	aa := a.Softmax(1)
+	bb := b.Softmax(1)
+	graph := pagerank.NewGraph()
+	x := NewMatrix(cols, len(vectors), make([]float32, cols*len(vectors))...)
+	y := NewMatrix(cols, len(vectors), make([]float32, cols*len(vectors))...)
+	for i := range vectors {
+		for ii, value := range vectors[i].Vector {
+			if value < 0 {
+				x.Data[i*cols+config.Size+ii] = -value
+				continue
+			}
+			x.Data[i*cols+ii] = value
+		}
+	}
+	for i := range vectors {
+		for ii, value := range vectors[i].Vector {
+			if value < 0 {
+				y.Data[i*cols+config.Size+ii] = -value
+				continue
+			}
+			y.Data[i*cols+ii] = value
+		}
+	}
+
+	xx := aa.MulT(x).Unit()
+	yy := bb.MulT(y).Unit()
+	cs := yy.MulT(xx)
+	for iteration := range config.Iterations {
+		for i := range cs.Rows {
+			for ii := range cs.Cols {
+				graph.Link(uint32(i), uint32(ii), float64(cs.Data[i*cs.Cols+ii])+.01*float64(rng.Float32()))
+			}
+		}
+
+		result := make([]float64, len(vectors))
+		graph.Rank(1.0, 1e-3, func(node uint32, rank float64) {
+			result[node] = rank
+		})
+		results[iteration] = result
+	}
+	for _, result := range results {
+		for i, value := range result {
+			vectors[i].Avg += value
+		}
+	}
+	for i, value := range vectors {
+		vectors[i].Avg = value.Avg / float64(config.Iterations)
+	}
+
+	for _, result := range results {
+		for i, value := range result {
+			diff := value - vectors[i].Avg
+			vectors[i].Stddev += diff * diff
+		}
+	}
+	for i, value := range vectors {
+		vectors[i].Stddev = math.Sqrt(value.Stddev / float64(config.Iterations))
+	}
+
+	cov := make([][]float64, len(vectors))
+	for i := range cov {
+		cov[i] = make([]float64, len(vectors))
+	}
+	for _, measures := range results {
+		for i, v := range measures {
+			for ii, vv := range measures {
+				diff1 := vectors[i].Avg - v
+				diff2 := vectors[ii].Avg - vv
+				cov[i][ii] += diff1 * diff2
+			}
+		}
+	}
+	if len(results) > 0 {
+		for i := range cov {
+			for ii := range cov[i] {
+				cov[i][ii] = cov[i][ii] / float64(len(results))
+			}
+		}
+	}
+	return cov
+}
+
 func MorpheusMarkov[T any, F Float](seed int64, config Config, vectors []*Vector[T]) Matrix[F] {
 	rng := rand.New(rand.NewSource(seed))
 	width := config.Size
